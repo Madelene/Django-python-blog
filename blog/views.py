1253a0b069019f60aa52a2258b10f1377bd4b22f
Madelene Campos <madelenecampos@gmail.com>
2018-02-20 14:37:58 -0500

    Imports reverse_lazy

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from . models import Post
 
 
