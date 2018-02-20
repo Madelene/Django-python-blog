fdd16d8348456ad028fd72b4ebef92c14904b56c
Madelene Campos <madelenecampos@gmail.com>
2018-02-20 14:34:25 -0500

    Updates views and makes sure URL redirect does not execute until our view has finished deleting the post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from . models import Post
 
 
@@ -17,9 +17,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
-    
+
 
 class BlogUpdateView(UpdateView):
     model = Post
     fields = ['title', 'body']
     template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
